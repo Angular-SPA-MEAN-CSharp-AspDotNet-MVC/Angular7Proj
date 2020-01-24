c4d0218c7b90bf8660c2d2dd06577ee4c9f9af2d
KH <knhg2013@gmail.com>
2020-01-23 22:24:34 -0500

    1)Complete the emp forms, 2)Add the customized validator

---
+++ src/app/employees/employee/employee.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from 'src/app/shared/employee.service';
+import { NgForm } from '@angular/forms';
+import { from } from 'rxjs';
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent implements OnInit {
+
+  constructor(private service: EmployeeService) { }
+
+  ngOnInit() {
+    this.resetForm();
+  }
+
+  resetForm(form?: NgForm){
+    if(form!=null)
+      form.resetForm();
+    this.service.formData  = {
+      EmployeeID: null,
+      FullName:'',
+      EMPCode:'',
+      Mobile:'',
+      Position:''
+    }
+  }
+}
